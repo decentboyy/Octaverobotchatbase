a894cef7b9198c67cc12b9a28d5b0edbf5b3e790
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-27 21:36:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+import logging
 import telebot
 
-# Replace 'TOKEN' with your bot token
-bot_token = '5261306325:AAFOSILKBJp2Uv5q-NwfVxHJAySgcI148Y8'
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Replace 'GROUP_ID' with the ID of the group where the bot will save the chats
-log_group_id = '-1001832126466'
+# Telegram Bot Token
+TOKEN = '5261306325:AAFOSILKBJp2Uv5q-NwfVxHJAySgcI148Y8'
 
-# Create a TeleBot instance
-bot = telebot.TeleBot(bot_token)
+# Private group ID to forward messages
+PRIVATE_GROUP_ID = '-1001832126466'  # Replace with the actual private group ID
 
-# Register a message handler for all incoming messages in any group
+# Create bot instance
+bot = telebot.TeleBot(TOKEN)
+
+# Handler to forward messages to private group
 @bot.message_handler(func=lambda message: True)
-def save_chat(message):
-    chat_id = message.chat.id
-    chat_message = f"{message.from_user.username}: {message.text}"
+def forward_message(message):
+    # Forward message to private group
+    bot.forward_message(chat_id=PRIVATE_GROUP_ID,
+                        from_chat_id=message.chat.id,
+                        message_id=message.message_id)
 
-    # Send the formatted chat message to the log group
-    bot.send_message(log_group_id, chat_message)
+# Handler for /start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello! I will forward all messages to the private group.')
 
-# Start the bot
+# Handler for unknown commands
+@bot.message_handler(func=lambda message: True)
+def unknown(message):
+    bot.reply_to(message, "Sorry, I didn't understand that command.")
+
+# Start the Bot
 bot.polling()
